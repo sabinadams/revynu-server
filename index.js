d9fdf302566eea8440cffd5651077101acc17c0a
M̰͒o̳̲̭͓̗͈̍͟n͐̄ͪͥͧk̉ <sabintheworld@gmail.com>
2017-04-07 22:57:19 -0700

    Routing File

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 var express = require('express'),
 app = express();
+
+var routes = require('./routes');
+
 app.use(express.static('www'));
+app.use('/', routes);
 app.set('port', process.env.PORT || 5000);
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
+
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
-});
-app.get('/', function (req, res) {
-  res.send('GET request to the homepage')
-});
-
-app.get('/test', (req, res) => {
-	res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ a: 1 }));
 });
